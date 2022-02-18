c73521991f71191dd0f60b215d5cde595e58aeac
Water-OR <3187299684@qq.com>
2022-02-18 21:54:36 +0800

    2022.02.18-21:54:update_everything

---
+++ auto/auto-push.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo git push -u origin main:main
+git push -u origin main:main
+
+if "%errorlevel%" == "0" ( goto GoodGame )
+
+:BadAction
+echo End of this round
+pause > nul
+cls
+echo git-push
+echo I'm sorry, but the pushing failed :[
+set /p aks=Auto push again(y/n):
+if /I "%aks%" EQU "n" (
+  cls
+  echo Push By yourself :]
+  set /p aks=git push
+  git push%aks%
+) else (
+  echo git push -u origin main:main
+  git push -u origin main:main
+)
+if "%errorlevel%" == "0" ( cls && goto GoodGame ) else ( goto BadAction )
+
+
+:GoodGame
+echo Pushing Successful!
+
+:Ending
+pause > nul
+set aks=
+exit
